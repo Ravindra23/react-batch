8cd23fc8074e3ee3fb539065939d61dcff44a7a1
Ravindra Pal SIngh <ravindradp2015@gmail.com>
2019-05-08 15:49:53 +0530

    Day 1: Intro to React

---
+++ src/Components/ClassComponent.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+class ClassComponent extends Component {
+    render() {
+        return (
+            <div>
+            <p>
+                hello world
+            </p>
+            <p>
+                hello Akash
+            </p>
+            </div>
+        )
+    }
+
+}
+
+export default ClassComponent;
---
+++ src/Components/FunctionalComponent.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const FunctionalComponent = () => {
+    return (
+        <div>
+            <p className="text-center" style={{fontSize:"30px"}}>
+                hello world
+            </p>
+            <input   type="text" placeholder="name" ></input>
+            <p>
+                hello Functional Component
+            </p>
+        </div>
+    )
+}
+
+export default FunctionalComponent;
